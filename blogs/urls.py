f35be6786ddcf3890524cf5e23fc1095c66e174a
jinesh0610 <jineshmodi562@gmail.com>
2023-09-12 13:46:46 +0530

    Intital-1 Commit

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,6 +4,8 @@ from blogs import views
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name ='home'),
-    path('blog/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail')
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
     
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from blogs.models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 # Create your views here.
 class BlogListView(ListView):
@@ -10,3 +10,13 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author','body']
+    
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
